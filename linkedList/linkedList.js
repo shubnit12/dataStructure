7ed0b5a5884b945433e12e2467327d3a58730ad5
shubnit12 <shubnit12@gmail.com>
2024-07-07 18:56:46 +0530

    a

--- linkedList/linkedList.js
+++ linkedList/linkedList.js
@@ -2,19 +2,19 @@ console.log("linked List")
 
 class Node{
     constructor(value){
-        this.value=value
-        this.next=null
+        this.value=value;
+        this.next=null;
     }
 }
 
 class LinkedList{
     constuctor(value){
-        const newNode = new Node(value)
-        this.head = newNode
-        this.tail = newNode
-        this.length = 1
+        const newNode = new Node(value);
+        this.head = newNode;
+        this.tail =  this.head;
+        this.length = 1;
     }
 
 }
-let mylinkedList = new LinkedList()
-console.log(mylinkedList)
+let mylinkedList = new LinkedList(13);
+console.log(mylinkedList);

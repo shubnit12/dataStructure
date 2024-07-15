40c6988046d6617c9096f49aefc65c5586bfd76c
shubnit12 <shubnit12@gmail.com>
2024-07-15 13:04:52 +0530

    DoublyLinkedList

---
+++ doublyLL/doublyll.js
@@ -0,0 +1,54 @@
+console.log("Doubly Linked List")
+
+class Node{
+ constructor(value){
+    this.value = value
+    this.next = null
+    this.prev = null
+ }
+}
+class DoublyLinkedList{
+    constructor(value){
+        let newNode = new Node(value)
+        this.head = newNode
+        this.tail = newNode
+        this.length = 1
+    }
+    push(value){
+        let newNode = new Node(value)
+        if(!this.head){
+            this.head=newNode
+            this.tail=newNode
+        }else{
+            this.tail.next=newNode
+            newNode.perv=this.tail
+            this.tail=newNode
+        }
+            this.length++
+            return this
+        
+
+    }
+    pop(){
+        if(this.length===0){
+            return undefined
+        }
+        let temp = this.tail
+        if(this.length===1){
+            this.head=null
+            this.tail=null
+        }else{
+            
+            this.tail=this.tail.perv
+            this.tail.next=null
+            temp.perv=null
+            
+        }
+
+        this.length--;
+        return temp
+    }
+}
+
+let myDLL = new DoublyLinkedList(13)
+console.log(myDLL)

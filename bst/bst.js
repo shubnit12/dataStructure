9eca711d025d2a344b6defd3c7f5d05bc9832b43
shubnit12 <shubnit12@gmail.com>
2024-07-18 22:28:00 +0530

    bst

--- bst/bst.js
+++ bst/bst.js
@@ -39,6 +39,33 @@ class Bst{
             
         }
     }
+    contain(value){
+        if(this.root===null){
+            return undefined
+        }
+        let temp = this.root
+        while(temp){
+            if(temp.value===value){
+                return true
+            }else if(temp.value<value){
+                temp=temp.right
+            }else{
+                temp=temp.left
+            }
+        }
+        return false
+    }
 }
 let myTree = new Bst()
-myTree 
+
+myTree.insert(47)
+
+myTree.insert(21)
+
+myTree.insert(76)
+myTree.insert(47)
+myTree.insert(18)
+myTree.insert(52)
+myTree.insert(82)
+
+myTree.insert(27)
